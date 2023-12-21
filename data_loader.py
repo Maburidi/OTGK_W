f7276add3f68737bf5c169472773abb347d5193f
Mohammed Aburidi <maburidi@gmail.com>
2023-12-21 13:57:30 -0800

    enzymes

--- data_loader.py
+++ data_loader.py
@@ -42,3 +42,30 @@ def build_MUTAG_dataset(path,one_hot=False):
 
 
 
+
+def build_ENZYMES_dataset(path,type_attr='label',use_node_deg=False):
+    graphs=graph_label_list(path,'ENZYMES_graph_labels.txt')
+    if type_attr=='label':
+        node_dic=node_labels_dic(path,'ENZYMES_node_labels.txt') # A voir pour les attributes
+    if type_attr=='real':
+        node_dic=node_attr_dic(path,'ENZYMES_node_attributes.txt')
+    adjency=compute_adjency(path,'ENZYMES_A.txt')
+    data_dict=graph_indicator(path,'ENZYMES_graph_indicator.txt')
+    data=[]
+    for i in graphs:
+        g=Graph()
+        for node in data_dict[i[0]]:
+            g.name=i[0]
+            g.add_vertex(node)
+            if not use_node_deg:
+                g.add_one_attribute(node,node_dic[node])
+            for node2 in adjency[node]:
+                g.add_edge((node,node2))
+        if use_node_deg:
+            node_degree_dict=dict(g.nx_graph.degree())
+            normalized_node_degree_dict={k:v/len(g.nx_graph.nodes()) for k,v in node_degree_dict.items() }
+            nx.set_node_attributes(g.nx_graph,normalized_node_degree_dict,'attr_name')
+        data.append((g,i[1]))
+
+    return data
+
